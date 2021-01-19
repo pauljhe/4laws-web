bc5092bab0be2f2c3dfe5cad2b6e6a5f91c26488
Paul Jhe <windrose93@hanmail.net>
2021-01-19 16:43:44 -0500

    remove unused and buggy class from greet0

--- src/pages/FourLaws/components/LawFour/LawFourGreet0/LawFourGreet0.tsx
+++ src/pages/FourLaws/components/LawFour/LawFourGreet0/LawFourGreet0.tsx
@@ -8,7 +8,7 @@ import { FourLawsPaths } from '../../../../paths';
 const LawFourGreet0: React.FC = () => {
   return (
     <React.Fragment>
-      <PageText type={PageTextType.CONTENT} className="LawFour-Wrapup Law-Sentence-Bold">
+      <PageText type={PageTextType.CONTENT} className="Law-Sentence-Bold">
         <FormattedMessage 
           id="fourlaws.law4.greet.zero"
           defaultMessage={'These two circles represent two kinds of lives:'}
